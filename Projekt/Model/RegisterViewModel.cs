74822d215e8386c7868a1afba810de5f9a9771c2
Konserwa203 <61764341+Konserwa203@users.noreply.github.com>
2020-09-06 21:39:09 +0200

    Add files via upload

---
+++ Projekt/Model/RegisterViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace WPF_Projekt_Filmy.Model
+{
+    public class RegisterViewModel : IDataErrorInfo
+    {
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(columnName) || columnName == nameof(RegisterLogin))
+                {
+                    if (string.IsNullOrWhiteSpace(RegisterLogin))
+                    {
+                        return "Login jest wymagany";
+                    }
+                    if (RegisterLogin.Length < 3)
+                    {
+                        return "Login musi posiadać przynajmniej 3 znaki";
+                    }
+                   
+                }
+                if (string.IsNullOrWhiteSpace(columnName) || columnName == nameof(RegisterPassword))
+                {
+                    if (string.IsNullOrWhiteSpace(RegisterPassword))
+                    {
+                        return "Hasło jest wymagane";
+                    }
+                    if (RegisterPassword.Length < 6)
+                    {
+                        return "Hasło musi posiadać przynajmniej 6 znaków";
+                    }
+                }
+                
+                return null;
+
+            }
+        }
+        public string Error => this[string.Empty];
+        public string RegisterLogin { get; set; }
+        public string RegisterPassword { get; set; }
+      
+
+
+    }
+}
